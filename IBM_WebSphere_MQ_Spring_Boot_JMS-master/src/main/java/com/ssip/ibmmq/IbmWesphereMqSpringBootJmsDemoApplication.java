18202eb2e65c8f00db26b564e60c39b462ed500b
Iswar <iswar.java9@gmail.com>
2018-08-21 08:17:53 +0530

    Initial commit IBM MQ integration without mq spring boot starter

---
+++ IBM_WebSphere_MQ_Spring_Boot_JMS-master/src/main/java/com/ssip/ibmmq/IbmWesphereMqSpringBootJmsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ssip.ibmmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmWesphereMqSpringBootJmsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbmWesphereMqSpringBootJmsDemoApplication.class, args);
+    }
+}
---
+++ IBM_WebSphere_MQ_Spring_Boot_JMS-master/src/main/java/com/ssip/ibmmq/Order.java
@@ -0,0 +1,24 @@
+package com.ssip.ibmmq;
+
+public class Order {
+
+	private int id;
+	private String desc;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", desc=" + desc + "]";
+	}
+	
+}
